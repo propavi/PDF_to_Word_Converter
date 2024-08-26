1f03cb1ef68755ac1d18ff6a28728a2222a7dd9c
Pavithran <119865636+propavi@users.noreply.github.com>
2024-08-27 00:42:26 +0530

    pdf_to_word.py

--- pdf_to_word.py
+++ pdf_to_word.py
@@ -6,29 +6,30 @@ import io
 pdf = None
 st.title("PDF to Word Converter")
 st.write("Upload your PDF here:")
-uploaded_file = st.file_uploader(" ",type = 'pdf')
+uploaded_file = st.file_uploader(" ", type='pdf')
 
 if uploaded_file is not None:
-    pdf = PdfReader(uploaded_file)
-    st.write("File Uploaded Successfully")
+    try:
+        pdf = PdfReader(uploaded_file)
+        st.write("File Uploaded Successfully")
+        st.write("No of Pages", len(pdf.pages))
 
-st.write("No of Pages",len(pdf.pages))
+        doc = Document()
 
-doc = Document()
+        for page_num in range(len(pdf.pages)):
+            page = pdf.pages[page_num]
+            text = page.extract_text()
+            doc.add_paragraph(text)
 
-for page_num in range(len(pdf.pages)):
-    page = pdf.pages[page_num]
-    text = page.extract_text()
-    doc.add_paragraph(text)
+        word_io = io.BytesIO()
+        doc.save(word_io)
+        word_io.seek(0)
 
-word_io = io.BytesIO()
-doc.save(word_io)
-word_io.seek(0)
-
-st.download_button(
-    label="Download Converted File",
-    data=word_io,
-    file_name="Converted.docx",
-    mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-)
-
+        st.download_button(
+            label="Download Converted File",
+            data=word_io,
+            file_name="Converted.docx",
+            mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        )
+    except AttributeError as e:
+        st.error(f"An error occurred: {e}")
